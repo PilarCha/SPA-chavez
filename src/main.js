c02595264a32ad0ae3e80ee8c4cd80a70a44e169
Pilar Chavez <pchavez92@gmail.com>
2018-03-20 18:17:31 -0700

    vuetify and bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
