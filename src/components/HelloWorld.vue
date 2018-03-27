32322464b6fc8ac83ec547395fae7bb4c6b171e1
Pilar Chavez <pchavez92@gmail.com>
2018-03-26 20:51:49 -0700

    refactoring. Code seaperated into different components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,18 @@
 <template lang="html">
-<b-navbar toggleable="md" type="dark" variant="dark">
-
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-  <b-navbar-brand style="font-size:1.75em;" href="#">Chavez Landscaping</b-navbar-brand>
-
-  <b-collapse is-nav id="nav_collapse">
-
-    <b-navbar-nav>
-      <b-nav-item href="#">About Us</b-nav-item>
-      <b-nav-item href="#" disabled>Services Provided</b-nav-item>
-      <b-nav-item href="#" disabled>Service Location</b-nav-item>
-      <b-nav-item href="#" disabled>Contact Form</b-nav-item>
-<b-nav-item href="#" disabled>Services Provided</b-nav-item> </b-navbar-nav>
-
-    <!-- Right aligned nav items -->
-
-  </b-collapse>
-</b-navbar>
+  <div>
+  <Navbar></Navbar>
+  <Aboutus></Aboutus>
+  </div>
 </template>
 
 <script>
+import Navbar from './nav-bar.vue'
+import Aboutus from './about-us.vue'
 export default {
+  components: {
+    Navbar,
+    Aboutus
+  }
 }
 </script>
 
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <b-navbar toggleable="md" type="dark" variant="dark">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand style="font-size:1.75em;" href="#">Chavez Landscaping</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-item href="#">About Us</b-nav-item>
+        <b-nav-item href="#">Services Provided</b-nav-item>
+        <b-nav-item href="#">Service Location</b-nav-item>
+        <b-nav-item href="#">Contact Form</b-nav-item>
+        <b-nav-item href="#">Services Provided</b-nav-item> </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
