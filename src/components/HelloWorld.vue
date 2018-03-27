eb5f3af3d22c9686b34e72d374423bbfda17ad5c
Pilar Chavez <pchavez92@gmail.com>
2018-03-26 20:28:12 -0700

    Changed burger

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,18 @@
 <template lang="html">
-  <b-navbar toggleable="md" type="dark" variant="dark">
+<b-navbar toggleable="md" type="dark" variant="dark">
 
   <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand style="font-size:1.75em;" href="#">Chavez Landscaping</b-navbar-brand>
 
   <b-collapse is-nav id="nav_collapse">
 
     <b-navbar-nav>
-      <b-nav-item >About Us</b-nav-item>
-      <b-nav-item >Services Provided</b-nav-item>
-      <b-navbar-brand href="#">Chavez Landscaping</b-navbar-brand>
-      <b-nav-item >Service Location</b-nav-item>
-      <b-nav-item >Contact Form</b-nav-item>
-    </b-navbar-nav>
+      <b-nav-item href="#">About Us</b-nav-item>
+      <b-nav-item href="#" disabled>Services Provided</b-nav-item>
+      <b-nav-item href="#" disabled>Service Location</b-nav-item>
+      <b-nav-item href="#" disabled>Contact Form</b-nav-item>
+<b-nav-item href="#" disabled>Services Provided</b-nav-item> </b-navbar-nav>
 
     <!-- Right aligned nav items -->
 
@@ -25,4 +26,5 @@ export default {
 </script>
 
 <style lang="css">
+
 </style>
